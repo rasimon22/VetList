cb6624894ff21b6589405dad3c945642b980204f
Rick Simon <rasimon@Ricks-MacBook-Pro.local>
2018-01-03 02:19:59 -0600

    basic db functionality working, can display content from local db. Ready for next sprint

--- views.py
+++ views.py
@@ -1,5 +1,26 @@
 from app import app
+from dbconnection import connection
 @app.route('/')
 def index():
-	return "<h1>This is the index</h1>"
+    return '<h1>This is the index</h1>'
+@app.route('/register', methods =["GET", "POST"])
+def register_page():
+    try:
+        cursor, db  = connection()
+        x = cursor.execute("SELECT *  FROM users")
+        #result = cursor.fetchone()
+        #result = cursor.fetchone()
+        result = cursor.fetchall()
+        if result:
+            string  = ''
+            for z in result:
+                string  += '<p>'+str(z[1]) + str(z[4]) + ' ' +'</p>'
+        return string
+        if result:
+            resultSet = []
+            for y in result:
+                resultSet+= y
+        return resultSet 
+    except Exception as e:
+        return (str(e))
 
