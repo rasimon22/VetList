0e229c329f7b730021f8401943301cb9387351ff
Rick Simon <rasimon@Ricks-MacBook-Pro.local>
2018-01-04 23:08:53 -0500

    login template created, ready to add CRUD. Basic form validation implemented

--- views.py
+++ views.py
@@ -1,28 +1,28 @@
 from app import app
 from dbconnection import connection
-from flask import render_template
+from flask import render_template, request
+from registerForm import SignupForm
 @app.route('/')
 @app.route('/<name>')
 def index(name = None):
-    return render_template('splash.html', name=name)
-@app.route('/register', methods =["GET", "POST"])
-def register_page():
-    try:
-        cursor, db  = connection()
-        x = cursor.execute("SELECT *  FROM users")
-        #result = cursor.fetchone()
-        #result = cursor.fetchone()
+    try: 
+        cursor, db = connection()
+        x = cursor.execute("Select employer.name, listings.description from listings, employer WHERE listings.employer_id = employer.employer_id;")
         result = cursor.fetchall()
         if result:
-            string  = ''
-            for z in result:
-                string  += '<p>'+str(z[1]) + str(z[4]) + ' ' +'</p>'
-        return string
-        if result:
-            resultSet = []
-            for y in result:
-                resultSet+= y
-        return resultSet 
-    except Exception as e:
-        return (str(e))
-
+            return render_template('splash.html', name=name, listings=result)
+        else:
+            return "Error"
+    except Exception as e: 
+        return "Database Error"
+@app.route('/register', methods =["GET"])
+def register_page(): 
+    return render_template('register.html', form=SignupForm(csrf_enabled=False))
+@app.route('/register', methods=["POST"])
+def register_post():
+    data=request.form
+    form =SignupForm(request.form, csrf_enabled=False)
+    if form.validate():
+        return data['username']
+    else:
+        return render_template('register.html', form=form) 
